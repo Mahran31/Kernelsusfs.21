c8a5d7d4986790c97919c340c869be6c081ff1c2
Toor Ali <toorali11@gmail.com>
2026-04-01 02:35:11 +0200

    remove code for BBG

--- fs/read_write.c
+++ fs/read_write.c
@@ -616,10 +616,6 @@ extern __attribute__((cold)) int ksu_handle_sys_read(unsigned int fd,
 
 SYSCALL_DEFINE3(read, unsigned int, fd, char __user *, buf, size_t, count)
 {
-#ifdef CONFIG_KSU
-	if (unlikely(ksu_vfs_read_hook)) 
-		ksu_handle_sys_read(fd, &buf, &count);
-#endif	
 	struct fd f = fdget_pos(fd);
 	ssize_t ret = -EBADF;
 
@@ -630,6 +626,10 @@ SYSCALL_DEFINE3(read, unsigned int, fd, char __user *, buf, size_t, count)
 			file_pos_write(f.file, pos);
 		fdput_pos(f);
 	}
+#ifdef CONFIG_KSU
+	if (unlikely(ksu_vfs_read_hook)) 
+		ksu_handle_sys_read(fd, &buf, &count);
+#endif		
 	return ret;
 }
 
